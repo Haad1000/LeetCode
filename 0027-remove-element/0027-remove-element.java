72a734f85a8f8d223ecd3afeb9ef17ef53d33210
Haad Ahmed Cheema <115592978+Haad1000@users.noreply.github.com>
2026-01-07 02:23:02 -0600

    Time: 0 ms (100%), Space: 43.8 MB (9.31%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,27 +1,18 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+
         int counter = 0;
+        int[] newArray = new int[nums.length];
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val) {
+            if (nums[i] != val) {
+                newArray[counter] = nums[i];
                 counter++;
             }
         }
-        int[] withVal = new int[counter];
-        int valCounter = 0;
-        int[] wihtoutVal = new int[nums.length - counter];
-        int nonValCounter = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val) {
-                withVal[valCounter] = nums[i];
-                valCounter++;
-            }
-            else {
-                wihtoutVal[nonValCounter] = nums[i];
-                nonValCounter++;
-            }
-        }
-        System.arraycopy(wihtoutVal, 0, nums, 0, nonValCounter);
-        System.arraycopy(withVal, 0, nums, nonValCounter, valCounter);
-        return nums.length - counter;
+
+        System.arraycopy(newArray, 0, nums, 0, newArray.length);
+
+        return counter;
     }
 }

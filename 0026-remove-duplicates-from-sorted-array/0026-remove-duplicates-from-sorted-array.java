5f8198290be242ab5070dc81cb0c024366a656eb
Haad Ahmed Cheema <115592978+Haad1000@users.noreply.github.com>
2026-01-06 22:01:20 -0600

    Time: 1 ms (74.28%), Space: 47 MB (10.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -5,15 +5,16 @@ class Solution {
             return 0;
         }
 
-        int i = 1;
+        int j = 1;
 
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i-1]) {
-                nums[i] = nums[j];
-                i++;
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[j-1] != nums[i]) {
+                nums[j] = nums[i];
+                j++;
             }
         }
 
-        return i;
+        return j;
     }
 }
